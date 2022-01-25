84831e56918186a5eb3aa514f66358d82f9b1a78
eboriley <eboriley@hotmail.co.uk>
2022-01-25 13:34:31 +0000

    removed announce winner function

--- dist/index.js
+++ dist/index.js
@@ -1,50 +1,60 @@
 "use strict";
 const items = ["🪨", "📃", "✂️"];
-function randomItem(item) {
-    const randomInt = Math.floor(Math.random() * 3);
-    return item[randomInt];
+function randomItem(items) {
+  const randomInt = Math.floor(Math.random() * 3);
+  return items[randomInt];
 }
 let hand1 = randomItem(items);
 let hand2 = randomItem(items);
 let pair = [hand1, hand2];
 console.log(pair);
 pair = pair.sort();
+//initialize all possible outcomes
 const outcomes = [
-    { winner: "🪨", combination: ["🪨", "✂️"] },
-    { winner: "✂️", combination: ["📃", "✂️"] },
-    { winner: "📃", combination: ["🪨", "📃"] }
+  { winner: "🪨", combination: ["🪨", "✂️"] },
+  { winner: "✂️", combination: ["📃", "✂️"] },
+  { winner: "📃", combination: ["🪨", "📃"] },
 ];
-outcomes.forEach(x => {
-    x.combination.sort();
-    if (pair[0] === pair[1]) {
-        console.log("Its a tie");
-    }
-    if (pair[0] === x.combination[0] && pair[1] === x.combination[1]) {
-        announceWinner(x);
-    }
+
+outcomes.forEach((x) => {
+  x.combination.sort();
+  //checks if both hands are the same
+  if (pair[0] === pair[1]) {
+    console.log("Its a tie");
+  }
+  //gets the combination among the possible outcomes
+  if (pair[0] === x.combination[0] && pair[1] === x.combination[1]) {
+    pair.forEach((p) => {
+    //checks which hand is the winner among the combition
+      if (x.winner === p) {
+        console.log(p + " won");
+      }
+    });
+  }
 });
-function announceWinner(outcome) {
-    if (outcome.winner === "🪨") {
-        if (hand1 === "🪨") {
-            console.log("hand one wins");
-        }
-        if (hand2 === "🪨")
-            console.log("hand two wins");
-    }
-    if (outcome.winner === "✂️") {
-        if (hand1 === "✂️") {
-            console.log("hand one wins");
-        }
-        if (hand2 === "✂️") {
-            console.log("hand two wins");
-        }
-    }
-    if (outcome.winner === "📃") {
-        if (hand1 === "📃") {
-            console.log("hand one wins");
-        }
-        if (hand2 === "📃") {
-            console.log("hand two wins");
-        }
-    }
-}
+
+// function announceWinner(outcome) {
+//     if (outcome.winner === "🪨") {
+//         if (hand1 === "🪨") {
+//             console.log("hand one wins");
+//         }
+//         if (hand2 === "🪨")
+//             console.log("hand two wins");
+//     }
+//     if (outcome.winner === "✂️") {
+//         if (hand1 === "✂️") {
+//             console.log("hand one wins");
+//         }
+//         if (hand2 === "✂️") {
+//             console.log("hand two wins");
+//         }
+//     }
+//     if (outcome.winner === "📃") {
+//         if (hand1 === "📃") {
+//             console.log("hand one wins");
+//         }
+//         if (hand2 === "📃") {
+//             console.log("hand two wins");
+//         }
+//     }
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,67 @@
 const items : string[] = ["🪨","📃","✂️"]
 
-function randomItem (item: string[]) : string {
-    const randomInt : number = Math.floor(Math.random() * 3)
-    return item[randomInt]
+function randomItem(items: string[]): string {
+  const randomInt: number = Math.floor(Math.random() * 3);
+  return items[randomInt];
 }
 
-let hand1 : string = randomItem(items)
-let hand2 : string = randomItem(items)
+let hand1: string = randomItem(items);
+let hand2: string = randomItem(items);
 
-let pair : string[] = [hand1,hand2]
-console.log(pair)
-pair = pair.sort()
+let pair: string[] = [hand1, hand2];
+console.log(pair);
+pair = pair.sort();
 
 interface Outcome {
-    winner: string
-    combination: string[]
+  winner: string;
+  combination: string[];
 }
 
-const outcomes : Outcome[] = [
-    {winner:"🪨" , combination :["🪨","✂️"] },
-    {winner:"✂️" , combination :["📃","✂️"] },
-    {winner:"📃" , combination :["🪨","📃"] }
-]
+const outcomes: Outcome[] = [
+  { winner: "🪨", combination: ["🪨", "✂️"] },
+  { winner: "✂️", combination: ["📃", "✂️"] },
+  { winner: "📃", combination: ["🪨", "📃"] },
+];
 
-outcomes.forEach(x => {
-    x.combination.sort()
-    if(pair[0] === pair[1]){
-        console.log("Its a tie")
-    }
-    if(pair[0] === x.combination[0] && pair[1] === x.combination[1]) {
-        announceWinner(x)
-    }
-})
+outcomes.forEach((x) => {
+  x.combination.sort();
+  if (pair[0] === pair[1]) {
+    console.log("Its a tie");
+  }
+  if (pair[0] === x.combination[0] && pair[1] === x.combination[1]) {
+    pair.forEach((p) => {
+      if (x.winner === p) {
+        console.log(p);
+      }
+    });
+  }
+});
 
-function announceWinner(outcome: Outcome) : void {
-    if(outcome.winner === "🪨"){
-        if(hand1 === "🪨"){
-            console.log("hand one wins")
-        }
-        if(hand2 === "🪨")
-        console.log("hand two wins")
+function announceWinner(outcome: Outcome): void {
+  if (outcome.winner === "🪨") {
+    if (hand1 === "🪨") {
+      console.log("hand one wins");
     }
+    if (hand2 === "🪨") console.log("hand two wins");
+  }
 
-    if(outcome.winner === "✂️"){
-        if(hand1 === "✂️"){
-            console.log("hand one wins")
-        }
-        if(hand2 === "✂️"){
-            console.log("hand two wins")
-        }
+  if (outcome.winner === "✂️") {
+    if (hand1 === "✂️") {
+      console.log("hand one wins");
     }
+    if (hand2 === "✂️") {
+      console.log("hand two wins");
+    }
+  }
 
-    if(outcome.winner === "📃"){
-        if(hand1 === "📃"){
-            console.log("hand one wins")
-        }
-        if(hand2 === "📃"){
-            console.log("hand two wins")
-        }
+  if (outcome.winner === "📃") {
+    if (hand1 === "📃") {
+      console.log("hand one wins");
     }
+    if (hand2 === "📃") {
+      console.log("hand two wins");
+    }
+  }
 }
 
 
